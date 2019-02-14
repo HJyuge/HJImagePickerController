8ba84f85271dc6c05f7ab87152f606304b56b179
HuangJin <huangjin@ecqun.com>
2019-02-14 17:54:09 +0800

    新增 # HJAssetCell子控件

--- HJImagePickerController/HJImagePickerController/HJImagePickerController/HJAssetCell.h
+++ HJImagePickerController/HJImagePickerController/HJImagePickerController/HJAssetCell.h
@@ -10,8 +10,21 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol UICollectionViewCellDelegate;
+
+
 @interface HJAssetCell : UICollectionViewCell
 @property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIImageView *selectedIndicator;
+@property (nonatomic, strong) UIImageView *maskImageView;
+
+- (void)setIndicatorState:(BOOL)state;
+@end
+
+@protocol UICollectionViewCellDelegate <NSObject>
+
+- (void)didSelectAssetCell:(HJAssetCell *)cell;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- HJImagePickerController/HJImagePickerController/HJImagePickerController/HJAssetCell.m
+++ HJImagePickerController/HJImagePickerController/HJImagePickerController/HJAssetCell.m
@@ -18,9 +18,23 @@
         self.imageView.contentMode = UIViewContentModeScaleAspectFill;
         self.imageView.clipsToBounds = YES;
         [self.contentView addSubview:self.imageView];
+        
+        self.selectedIndicator = [[UIImageView alloc]initWithFrame:CGRectMake(self.contentView.bounds.size.width - 10 - 5 , 5, 10, 10)];
+        self.selectedIndicator.contentMode = UIViewContentModeScaleAspectFill;
+        self.selectedIndicator.clipsToBounds = YES;
+        self.selectedIndicator.image = [UIImage imageNamed:@""];
+        [self.contentView addSubview:self.selectedIndicator];
+        
+        self.maskImageView = [[UIImageView alloc]initWithFrame:self.contentView.bounds];
+        self.maskImageView.image = [UIImage imageNamed:@""];
+        [self.contentView addSubview:self.maskImageView];
+        
     }
     return self;
 }
 
+- (void)setIndicatorState:(BOOL)state {
+    self.selectedIndicator.image = state?[UIImage imageNamed:@""]:[UIImage imageNamed:@""];
+}
 
 @end
